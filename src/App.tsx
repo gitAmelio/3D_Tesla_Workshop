84fb2fdd414df184934ca6c284d05d03cb7cb974
gitAmelio <ameliosconnection@gmail.com>
2023-10-28 09:15:26 +0200

    covered points

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Canvas } from '@react-three/fiber';
 import Box from './components/Box';
 import { OrbitControls } from '@react-three/drei';
+import CustomGeometryParticles from './components/Customparticles';
 
 const App = () => {
   return (
@@ -12,9 +13,12 @@ const App = () => {
         <Box position={[1,1,0]}/>
         <axesHelper args={[5]}/>
         <OrbitControls />
+        {/* <ambientLight intensity={0.5} /> */}
+        {/* <CustomGeometryParticles count={9} shape="box"/> */}
+        <CustomGeometryParticles count={2000} shape="sphere"/>
       </Canvas>
     </div>
   )
 }
 
-export default App;
+export default App; 
